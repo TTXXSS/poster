467e2a8965dc2fed60689a694894ee6e6e9b15bb
TTXXSS <1374230455@qq.com>
2019-06-16 19:36:29 +0800

    second commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function getMultiplyTable() {
+function getMultiplyTable() {  //输出99乘法表
     var str = "";
     for (var i = 1; i < 10; i++) {
         for (var j = 1; j < 10; j++) {
@@ -13,4 +13,14 @@ function getMultiplyTable() {
 
     }
     console.log(str);
+}
+
+function outputOddAndEven() {  //输出0-20之间的奇数和偶数
+    for (let i = 0; i <= 20; i++) {
+        if (i % 2 == 1) {
+            console.log(i + "是奇数。");
+        } else {
+            console.log(i + "是偶数。");
+        }
+    }
 }
