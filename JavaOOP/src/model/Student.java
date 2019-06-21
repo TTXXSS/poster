fee5c743b9fc6756839b4fb3640752e42cd72625
TTXXSS <1374230455@qq.com>
2019-06-21 13:36:22 +0800

    second commit

---
+++ JavaOOP/src/model/Student.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Student {
+    private String studentName;
+    private String studentNumber;
+    private int studentAge;
+    private String studentSex;
+
+    public Student() {
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public int getStudentAge() {
+        return studentAge;
+    }
+
+    public void setStudentAge(int studentAge) {
+        this.studentAge = studentAge;
+    }
+
+    public String getStudentSex() {
+        return studentSex;
+    }
+
+    public void setStudentSex(String studentSex) {
+        this.studentSex = studentSex;
+    }
+
+}
